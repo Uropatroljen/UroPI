1f191a862bfa8434ebe8a0e59b48f68aa5a9ff72
Daniel-Datatekniker <daniel.datateknikerprogrammoer@outlook.com>
2022-09-23 13:21:23 +0200

    Setup network

--- UroPI/NetworkUtils.py
+++ UroPI/NetworkUtils.py
@@ -35,13 +35,25 @@ class c_NetworkUtils :
             wifi.write(config)
         #displaying success
         print("wifi config added")
-    #TODO
-    def IsConnected(self):
-        try:
-            proc = subprocess.check_output(['sudo', 'iwgetid']).decode()
-        except :
-            return False
-        
+ 
+    def ConnectToWifi(self):
+        os.system("nmcli device wifi connect\""+self.configReader.GetSsid+"\" password \""+self.configReader.GetPsk+"\"")
+ 
+    def StartHostpot(self):
+        #sudo systemctl enable hostapd dnsmasq
+        #comment the static IP config in /etc/dhcpcd.conf
+        #sudo systemctl reboot
+        os.system("sudo systemctl enable hostapd dnsmasq")
+        os.system("echo $(cat /etc/dhcpcd.conf.hotspot) > /etc/dhcpcd.conf)")
+        os.system("echo $(cat /etc/dnsmasq.conf.hotspot) > /etc/dnsmasq.conf)")
+        os.system("systemctl reboot")
+
+    def DisableHotspot(self):
+        os.system("sudo systemctl disable hostapd dnsmasq")
+        os.system("cat /stc/dhcpcd.conf.orig | sudo tee a /etc/dhcpcd.conf >/dev/null")
+        os.system("cat /stc/dnsmasq.conf.orig | sudo tee a /etc/dnsmasq.conf >/dev/null")
+        os.system("systemctl reboot")
+      
     def get_ip_address(self) -> str:
         """Get ip address from INET (IPV4)"""
         #Af_Inet represent the address & protocol families.
